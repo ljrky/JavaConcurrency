85e93f2fd969dde7ef69e06a79a26267d4434c7b
keyu.ruan <kerua@microsoft.com>
2015-07-07 17:40:58 +0300

    add thread interference sample

---
+++ src/ThreadInterference.java
@@ -0,0 +1,74 @@
+public class ThreadInterference {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(new IncreaseCounter(counter));
+        Thread t2 = new Thread(new IncreaseCounter(counter));
+        Thread t3 = new Thread(new IncreaseCounter(counter));
+
+        Thread t4 = new Thread(new IncreaseCounter(counter));
+        Thread t5 = new Thread(new IncreaseCounter(counter));
+        Thread t6 = new Thread(new IncreaseCounter(counter));
+
+        t1.start();
+        t1.join();
+        System.out.println(counter.value());
+        t2.start();
+        System.out.println(counter.value());
+        t3.start();
+        System.out.println(counter.value());
+        t4.start();
+        System.out.println(counter.value());
+        t5.start();
+        System.out.println(counter.value());
+        t6.start();
+        System.out.println(counter.value());
+
+        Thread.sleep(3000);
+        System.out.println(counter.value());
+    }
+
+
+    static class Counter{
+    private int c = 0;
+
+    public void increment(){
+        c++;
+    }
+
+    public void decrement(){
+        c--;
+    }
+
+    public int value(){
+        return c;
+    }
+}
+
+    static class IncreaseCounter implements Runnable{
+
+        Counter counter;
+        public IncreaseCounter(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            this.counter.increment();
+        }
+    }
+
+    static class DecreaseCounter implements Runnable{
+
+        Counter counter;
+        public DecreaseCounter(Counter counter) {
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            this.counter.decrement();
+        }
+    }
+
+}
